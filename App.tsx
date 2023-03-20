8f33d86d2fef0272533cf6190145eb09af4ce638
Jude Tejada <judelaurentetejada@gmail.com>
2023-03-20 23:38:22 +0800

    init project and setup tamagui with expo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
 
+import { useColorScheme } from "react-native";
+
+import { H1, Paragraph, TamaguiProvider, Theme, YStack } from "tamagui";
+
+import { useFonts } from "expo-font";
+
+import config from "./tamagui.config";
 export default function App() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+  if (!loaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={config}>
+      <Theme name={colorScheme === "dark" ? "dark" : "light"}>
+        <YStack
+          f={1}
+          jc="center"
+          ai="center"
+          backgroundColor={"$backgroundSoft"}
+        >
+          <Paragraph color="$color" jc="center" fontWeight='InterBold'>
+            {colorScheme}
+          </Paragraph>
+
+          <StatusBar style="auto" />
+        </YStack>
+      </Theme>
+    </TamaguiProvider >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,32 @@
+process.env.TAMAGUI_TARGET = "native"; // Don't forget to specify your TAMAGUI_TARGET here
 module.exports = function(api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+
+    plugins: [
+      [
+        "@tamagui/babel-plugin",
+
+        {
+          components: ["tamagui"],
+
+          config: "./tamagui.config.ts",
+
+          logTimings: true,
+        },
+      ],
+
+      [
+        "transform-inline-environment-variables",
+
+        {
+          include: "TAMAGUI_TARGET",
+        },
+      ],
+
+      "react-native-reanimated/plugin",
+    ],
   };
 };
