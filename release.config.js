f4d59109f9ef13b60a14ec273f3ccd917f00969f
Jude Tejada <judelaurentetejada@gmail.com>
2023-03-22 10:42:46 +0800

    add semantic release and expo update

--- release.config.js
+++ release.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  branches: ['main'],
+  branches: ['master'],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
+    '@semantic-release/release-notes-generator',
+    {
+      name: '@semantic-release/npm',
+      options: {
+        // Add your NPM auth token here
+        npmAuthToken: process.env.NPM_TOKEN,
+      },
+    },
     '@semantic-release/git',
     '@semantic-release/changelog',
   ],
